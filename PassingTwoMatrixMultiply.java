582ebd296911c73e8c11da9b2f8557d22bd76c4f
Chithiraipavai_MN <chithiraipavaimn@gmail.com>
2022-02-10 14:04:25 +0530

    Programs

--- PassingTwoMatrixMultiply.java
+++ PassingTwoMatrixMultiply.java
@@ -2,48 +2,53 @@ import java.util.*;
 //  Program to multiply two matrices by passing matrix to a function
 public class PassingTwoMatrixMultiply
 {
-    public static void main(String[] args)
-    {
-        Scanner scan=new Scanner(System.in);
-        int i,j,k;
-        System.out.println("Enter N: ");
-        int N= scan.nextInt();
-        System.out.println("Enter M: ");
-        int M= scan.nextInt();
-        int[][] arr1=new int[N][M];
-        int[][] arr2=new int[M][N];
-        int[][] result=new int[N][N];
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i, j, k;
+        System.out.println("Enter N1: ");
+        int N1 = scan.nextInt();
+        System.out.println("Enter M1: ");
+        int M1 = scan.nextInt();
+        System.out.println("Enter N2: ");
+        int N2 = scan.nextInt();
+        System.out.println("Enter M2: ");
+        int M2 = scan.nextInt();
+        int[][] arr1 = new int[N1][M1];
+        int[][] arr2 = new int[N2][M2];
         System.out.println("Enter array1: ");
-        for(i=0;i<N;i++)
-        {
-            for(j=0;j<M;j++)
-            {
-                arr1[i][j]= scan.nextInt();
+        for (i = 0; i < N1; i++) {
+            for (j = 0; j < M1; j++) {
+                arr1[i][j] = scan.nextInt();
             }
         }
         System.out.println("Enter array2: ");
-        for(i=0;i<M;i++)
-        {
-            for(j=0;j<N;j++)
-            {
-                arr2[i][j]= scan.nextInt();
+        for (i = 0; i < N2; i++) {
+            for (j = 0; j < M2; j++) {
+                arr2[i][j] = scan.nextInt();
             }
         }
-        for(i=0;i<N;i++)
+        Multiply(arr1,arr2,N1,M1,N2,M2);
+
+    }
+    public static void Multiply(int[][] arr1,int[][] arr2,int N1,int M1,int N2,int M2)
+    {
+        int i,j,k;
+        int[][] result = new int[N1][M2];
+        for(i=0;i<N1;i++)
         {
-            for(j=0;j<N;j++)
+            for(j=0;j<M2;j++)
             {
                 result[i][j]=0;
-                for(k=0;k<M;k++)
+                for(k=0;k<M1;k++)
                 {
                     result[i][j]=result[i][j]+(arr1[i][k]*arr2[k][j]);
                 }
             }
         }
         System.out.println("Result is: ");
-        for(i=0;i<N;i++)
+        for(i=0;i<N1;i++)
         {
-            for(j=0;j<N;j++)
+            for(j=0;j<M2;j++)
             {
                 System.out.print(result[i][j]+"\t");
             }
@@ -55,10 +60,14 @@ public class PassingTwoMatrixMultiply
 
 Output:
 
-Enter N:
+Enter N1:
 2
-Enter M:
+Enter M1:
 3
+Enter N2:
+3
+Enter M2:
+2
 Enter array1:
 3
 -2
